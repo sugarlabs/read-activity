6392e0045cbc0a96a57f0139a3b230729099488d
Marco Pesenti Gritti <marco@localhost.localdomain>
2006-08-10 20:58:28 +0200

    Adapt to new api

--- XbookActivity.py
+++ XbookActivity.py
@@ -3,10 +3,9 @@ import gtk
 from sugar.activity.Activity import Activity
 
 class XbookActivity(Activity):
-        def __init__(self, service):
-                Activity.__init__(self, service)
+        def __init__(self):
+                Activity.__init__(self)
 
                 button = gtk.Button('drawing')
                 self.add(button)
                 button.show()
-
