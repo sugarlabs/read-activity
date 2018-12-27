sugarlabs/read-activity

commit a55a0cc6d70215ccb0d887df81555ef9edb09732

Replace BeautifulSoup with ElementTree
- removes dependency on BeautifulSoup 3,

- avoids BeautifulSoup 4 which is not available on Fedora 18,

- ElementTree already used elsewhere in activity and toolkit,

- test case; open an EPUB file and use the edit toolbar find entry.

Reported-by: György Balló <ballogyor@gmail.com>

epubview/jobs.py
@@ -21,7 +21,7 @@ from gi.repository import Gtk
 import widgets
 import math
 import os.path
-import BeautifulSoup
+import xml.etree.ElementTree as etree
 
 import threading
 
@@ -62,12 +62,20 @@ class SearchThread(threading.Thread):
         return False
 
     def _searchfile(self, fileobj):
-        soup = BeautifulSoup.BeautifulSoup(fileobj)
-        body = soup.find('body')
-        tags = body.findChildren(True)
-        for tag in tags:
-            if tag.string is not None:
-                if tag.string.lower().find(self.obj._text.lower()) > -1:
+        tree = etree.parse(fileobj)
+        root = tree.getroot()
+
+        body = None
+        for child in root:
+            if child.tag.endswith('body'):
+                body = child
+
+        if body is None:
+            return False
+
+        for child in body.iter():
+            if child.text is not None:
+                if child.text.lower().find(self.obj._text.lower()) > -1:
                     return True
 
         return False

commit 7d94212564a0d77996a43ccf3e180f0a86c49480

EPub: make the ET recognize HTML entities
Native HTML support in ElementTree never really worked and is marked
deprecated in Python 3. This means it blows up upon encountering an
entity that is not a XML entity. Sigh.

  Exception in thread Thread-1:
  Traceback (most recent call last):
    File "/usr/lib64/python2.7/threading.py", line 804, in __bootstrap_inner
      self.run()
    File "/home/lkundrak/epub/read-activity/epubview/jobs.py", line 90, in run
      self._start_search()
    File "/home/lkundrak/epub/read-activity/epubview/jobs.py", line 55, in _start_search
      if self._searchfile(f):
    File "/home/lkundrak/epub/read-activity/epubview/jobs.py", line 71, in _searchfile
      tree = etree.parse(fileobj, parser=parser)
    File "/usr/lib64/python2.7/xml/etree/ElementTree.py", line 1182, in parse
      tree.parse(source, parser)
    File "/usr/lib64/python2.7/xml/etree/ElementTree.py", line 656, in parse
      parser.feed(data)
    File "/usr/lib64/python2.7/xml/etree/ElementTree.py", line 1659, in feed
      self._raiseerror(v)
    File "/usr/lib64/python2.7/xml/etree/ElementTree.py", line 1523, in _raiseerror
      raise err
  ParseError: undefined entity &nbsp;: line 35, column 132

Fixes: a55a0cc6d70215ccb0d887df81555ef9edb09732

epubview/jobs.py
@@ -22,6 +22,7 @@ import widgets
 import math
 import os.path
 import xml.etree.ElementTree as etree
+import htmlentitydefs as html_entities
 
 import threading
 
@@ -62,7 +63,10 @@ class SearchThread(threading.Thread):
         return False
 
     def _searchfile(self, fileobj):
-        tree = etree.parse(fileobj)
+        parser = etree.XMLParser(html=1)
+        for name, codepoint in html_entities.name2codepoint.iteritems():
+            parser.entity[name] = unichr(codepoint)
+        tree = etree.parse(fileobj, parser=parser)
         root = tree.getroot()
 
         body = None