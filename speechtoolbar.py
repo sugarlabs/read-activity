c940eb1fbaba14490b7d93787750daf8bd97fe62
James Cameron <quozl@laptop.org>
2019-09-11 17:40:40 +1000

    Remove speech language
    Doesn't work on systems where language support packages are trimmed.

--- speechtoolbar.py
+++ speechtoolbar.py
@@ -35,28 +35,6 @@ class SpeechToolbar(Gtk.Toolbar):
         self._speech = SpeechManager()
         self._is_paused = False
 
-        self.load_speech_parameters()
-
-        self._voices = self._speech.get_all_voices()  # a dictionary
-
-        locale = os.environ.get('LANG', '')
-        language_location = locale.split('.', 1)[0].lower()
-        language = language_location.split('_')[0]
-        # if the language is es but not es_es default to es_la (latin voice)
-        if language == 'es' and language_location != 'es_es':
-            language_location = 'es_la'
-
-        self._voice = 'en_us'
-        if language_location in self._voices:
-            self._voice = language_location
-        elif language in self._voices:
-            self._voice = language
-
-        voice_names = []
-        for language, name in self._voices.iteritems():
-            voice_names.append((language, name))
-        voice_names.sort(self._compare_voice)
-
         # Play button
         self._play_button = ToggleToolButton('media-playback-start')
         self._play_button.show()
@@ -72,58 +50,7 @@ class SpeechToolbar(Gtk.Toolbar):
         self.insert(self._stop_button, -1)
         self._stop_button.set_tooltip(_('Stop'))
 
-        # Language list
-        combo = ComboBox()
-        which = 0
-        for pair in voice_names:
-            language, name = pair
-            combo.append_item(language, name)
-            if language == self._voice:
-                combo.set_active(which)
-            which += 1
-
-        combo.connect('changed', self._voice_changed_cb)
-        combotool = ToolComboBox(combo)
-        self.insert(combotool, -1)
-        combotool.show()
-
         self._speech.connect('stop', self._reset_buttons_cb)
-
-    def _compare_voice(self, a, b):
-        if a[1].lower() == b[1].lower():
-            return 0
-        if a[1].lower() < b[1].lower():
-            return -1
-        if a[1].lower() > b[1].lower():
-            return 1
-
-    def _voice_changed_cb(self, combo):
-        self._voice = combo.props.value
-        self._speech.say_text(self._voices[self._voice])
-        self.save_speech_parameters()
-
-    def load_speech_parameters(self):
-        speech_parameters = {}
-        data_path = os.path.join(self._activity.get_activity_root(), 'data')
-        data_file_name = os.path.join(data_path, 'speech_params.json')
-        if os.path.exists(data_file_name):
-            f = open(data_file_name, 'r')
-            try:
-                speech_parameters = json.load(f)
-                self._voice = speech_parameters['voice']
-            finally:
-                f.close()
-
-    def save_speech_parameters(self):
-        speech_parameters = {}
-        speech_parameters['voice'] = self._voice
-        data_path = os.path.join(self._activity.get_activity_root(), 'data')
-        data_file_name = os.path.join(data_path, 'speech_params.json')
-        f = open(data_file_name, 'w')
-        try:
-            json.dump(speech_parameters, f)
-        finally:
-            f.close()
 
     def _reset_buttons_cb(self, widget=None):
         self._play_button.set_icon_name('media-playback-start')
@@ -136,8 +63,7 @@ class SpeechToolbar(Gtk.Toolbar):
             self._play_button.set_icon_name('media-playback-pause')
             if not self._is_paused:
                 self._speech.say_text(
-                    self._activity._view.get_marked_words(),
-                    lang_code=self._voice)
+                    self._activity._view.get_marked_words())
             else:
                 self._speech.restart()
         else:
